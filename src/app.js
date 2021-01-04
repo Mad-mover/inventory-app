32dae51175d94d8cabf6c4f82827e1c2d681591d
Jack <jackranold.3@gmail.com>
2021-01-04 09:39:13 +0300

    master

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SCSS/app.scss";
+
+const App = () => {
+    return (
+        <div>
+            <h1>hello this is jack</h1>
+        </div>
+    );
+};
+
+export default App;
